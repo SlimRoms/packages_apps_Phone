c6cec5ec53bf98944aaeea4ae8849cd5455496ee
Danny Baumann <dannybaumann@web.de>
2013-09-04 11:45:03 +0200

    Fix ringer not stopping when call is blacklisted.
    We skipped quite a lot of post-call cleanup in the blacklist case,
    including stopping the ringer. Because of that, it depended on the
    timing of the first ring event and the disconnect event whether ringing
    was correctly suppressed or whether the phone rang indefinitely.
    
    Fix that by only skipping the steps that need to be skipped (mainly
    adding the call log entry).
    
    Change-Id: I11a7722d0560d84e0a6305d0c9788f8e316f771e

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -299,8 +299,8 @@ public class CallNotifier extends Handler
         switch (msg.what) {
             case PHONE_NEW_RINGING_CONNECTION:
                 log("RINGING... (new)");
-                onNewRingingConnection((AsyncResult) msg.obj);
                 mSilentRingerRequested = false;
+                onNewRingingConnection((AsyncResult) msg.obj);
                 break;
 
             case PHONE_INCOMING_RING:
@@ -488,6 +488,7 @@ public class CallNotifier extends Handler
             c.setUserData(BLACKLIST);
             try {
                 c.hangup();
+                silenceRinger();
                 mApplication.notificationMgr.notifyBlacklistedCall(number,
                         c.getCreateTime(), listType);
             } catch (CallStateException e) {
@@ -1137,9 +1138,17 @@ public class CallNotifier extends Handler
             Log.w(LOG_TAG, "onDisconnect: null connection");
         }
 
+        boolean disconnectedDueToBlacklist = false;
+
         if (c != null) {
             mForwardedCalls.remove(c);
             mWaitingCalls.remove(c);
+            disconnectedDueToBlacklist = BLACKLIST.equals(c.getUserData());
+
+            boolean vibHangup = PhoneUtils.PhoneSettings.vibHangup(mApplication);
+            if (!disconnectedDueToBlacklist && vibHangup && c.getDurationMillis() > 0) {
+                vibrate(50, 100, 50);
+            }
         }
 
         int autoretrySetting = 0;
@@ -1151,6 +1160,9 @@ public class CallNotifier extends Handler
         // Stop any signalInfo tone being played when a call gets ended
         stopSignalInfoTone();
 
+        // Stop 45-second vibration
+        removeMessages(VIBRATE_45_SEC);
+
         if ((c != null) && (c.getCall().getPhone().getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA)) {
             // Resetting the CdmaPhoneCallState members
             mApplication.cdmaPhoneCallState.resetCdmaPhoneCallState();
@@ -1158,20 +1170,6 @@ public class CallNotifier extends Handler
             // Remove Call waiting timers
             removeMessages(CALLWAITING_CALLERINFO_DISPLAY_DONE);
             removeMessages(CALLWAITING_ADDCALL_DISABLE_TIMEOUT);
-        }
-
-        if (c != null) {
-            Object o = c.getUserData();
-            if (BLACKLIST.equals(o)) {
-                if (VDBG) Log.i(LOG_TAG, "in blacklist so skip calllog");
-                return;
-            }
-
-            boolean vibHangup = PhoneUtils.PhoneSettings.vibHangup(mApplication);
-            if (vibHangup && c.getDurationMillis() > 0) {
-                vibrate(50, 100, 50);
-            }
-            removeMessages(VIBRATE_45_SEC);
         }
 
         // Stop the ringer if it was ringing (for an incoming call that
@@ -1291,7 +1289,9 @@ public class CallNotifier extends Handler
         }
 
         if (c != null) {
-            mCallLogger.logCall(c);
+            if (!disconnectedDueToBlacklist) {
+                mCallLogger.logCall(c);
+            }
 
             final String number = c.getAddress();
             final Phone phone = c.getCall().getPhone();
@@ -1339,6 +1339,7 @@ public class CallNotifier extends Handler
             if (((mPreviousCdmaCallState == Call.State.DIALING)
                     || (mPreviousCdmaCallState == Call.State.ALERTING))
                     && (!isEmergencyNumber)
+                    && !disconnectedDueToBlacklist
                     && (cause != Connection.DisconnectCause.INCOMING_MISSED )
                     && (cause != Connection.DisconnectCause.NORMAL)
                     && (cause != Connection.DisconnectCause.LOCAL)
